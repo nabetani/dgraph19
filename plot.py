44fda8ff3fd3de0d19e68ad13cd229669d2bb28c
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-03-29 18:11:46 +0900

    出力サイズ変更・棒グラフに変更

--- plot.py
+++ plot.py
@@ -13,6 +13,10 @@ def filenames():
 
 
 SQUARE_20cm = (20/2.54, 20/2.54)
+TWEET_PREVIEW_SISE = (20/2.54, 20/2.54*286/509)
+POPULATIONS = {
+    "Japan": 1.2595e8,
+}
 
 
 def ymd(v):
@@ -37,17 +41,16 @@ def getY(values, day):
 
 
 def plot_op(fig, datas):
-    fig.suptitle("COVID-19 death toll")
+    fig.suptitle("COVID-19 Daily New Deaths in Japan")
     graph = fig.add_subplot(1, 1, 1)  # nrows, ncols, index
     for key in datas:
         values = datas[key].values
         days = getDays(values)
         y = [getY(values, day) for day in days]
-        graph.plot(days, y, label=key)
+        graph.bar(days, y, label=key)
     labels = graph.get_xticklabels()
     pyplot.setp(labels, rotation=90)
-    graph.legend()
-    fig.subplots_adjust(top=0.9, bottom=0.15)
+    fig.subplots_adjust(top=0.9, bottom=0.25)
     fig.align_labels()  # 同一ページ内の y-label の位置などを揃える
 
 
@@ -57,7 +60,7 @@ def main():
     for key in fns:
         fn = fns[key]
         datas[key] = pd.read_csv(fn, usecols=[0, 1, 2, 3])
-    plot_op(pyplot.figure(figsize=SQUARE_20cm), datas)
+    plot_op(pyplot.figure(figsize=TWEET_PREVIEW_SISE), datas)
 
     pyplot.savefig('graph.png')
 
