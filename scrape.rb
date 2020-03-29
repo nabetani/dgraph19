f84c5c031dc42679721fa0a9414741db805af5d9
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-03-29 16:06:42 +0900

    字句解析

--- scrape.rb
+++ scrape.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 require 'pry'
+require 'pp'
 
 HERE = File.split( __FILE__ )[0]
 
@@ -13,23 +14,38 @@ end
 def parse( html )
 end
 
+def asciinum(s)
+  s.tr( "０１２３４５６７８９","0123456789" )
+end
+
+def jtoi(s)
+  return s if Integer===s
+  asciinum(s).to_i
+end
+
+def makerow(y,m,d,nums)
+  cases = asciinum(nums).scan( /\d+/).map(&:to_i)
+  [ jtoi(y), jtoi(m), jtoi(d), cases.size ] + cases
+end
+
 def main
   html = open(mhlw_url) do |f|
     f.set_encoding("utf-8")
     f.read
   end
   renum = "０１２３４５６７８９0123456789"
-  html.scan( %r![\(（]([#{renum}]+)月([#{renum}]+)日公表分[\)）].{0,10}（国内死亡\s*(.{1,20})例目!) do |m|
+  data = []
+  pat0 = %r![\(（]([#{renum}]+)月([#{renum}]+)日公表分[\)）].{0,10}（国内死亡\s*(.{1,20})例目!
+  html.scan(pat0) do |m|
     p m
+    data.push(makerow(2020,*(0..2).map{ |e| m[e] }))
   end
   puts "-"*10
-  html.scan( %r!([#{renum}]+)年([#{renum}]+)月([#{renum}]+)日[^年月日]{50,400}死亡(.{1,20})例!) do |m|
-    p m
+  pat1 =  %r!([#{renum}]+)年([#{renum}]+)月([#{renum}]+)日[^年月日]{50,400}死亡(.{1,20})例!
+  html.scan(pat1) do |m|
+    data.push(makerow(*(0..3).map{ |e| m[e] }))
   end
-
-
-
-
+  pp data.sort
 end
 
 main
